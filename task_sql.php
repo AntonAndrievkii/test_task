769b9fbe07fa0329de1d3be724f96e343be58630
AntonAndrievkii <48820730+AntonAndrievkii@users.noreply.github.com>
2024-02-10 20:04:49 +0300

    Update task_sql.php

--- task_sql.php
+++ task_sql.php
@@ -1,1 +1,8 @@
 
+SELECT DATE_FORMAT(gal.DATE, '%d-%m-%Y') AS Date,
+       bu.LOGIN AS User_Login,
+       gal.METHOD AS Method_Name,
+       COUNT(*) AS Method_Count
+FROM google_ads_logs gal
+JOIN b_user bu ON gal.USER_ID = bu.ID
+GROUP BY DATE, USER_Login, Method_Name;
